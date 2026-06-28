bc1f19567784f8997c6ae7f63887ec2d0ceb52a3
Ritesh Kawadkar <ritesh.kawadkar@gmail.com>
2026-06-28 13:57:02 +0530

    Updated

--- SDM.Shims/Dapper.cs
+++
@@ -1,8 +0,0 @@
-namespace Dapper
-{
-    // Minimal shim to satisfy testhost runtime binding. Not a real Dapper implementation.
-    public static class SqlMapper
-    {
-        public static void Dummy() { }
-    }
-}
